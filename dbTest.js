e2bb93af2f0012df74e47dc2b9efedb1b75f0fc4
Derek Ensign <derekensign@gmail.com>
2021-03-31 23:55:01 -0500

    completed the associations

--- dbTest.js
+++ dbTest.js
@@ -46,4 +46,84 @@ const models = require('./models/')
 // deleteDengar()
 // 
 
-models.hunter.create({name: 'Dengar', client: 'Mercurial Swift', active: false })
+// models.hunter.create({name: 'Dengar', client: 'Mercurial Swift', active: false })
+
+// let associateHan = async () => {
+//     let han = await models.bounty.findOne({
+//         where: {
+//             name: 'Han Solo'
+//         }
+//     })
+//     let boba = await models.hunter.findOne({
+//         where: {
+//             name: 'Boba Fett'
+//         }
+//     })
+//     han.addHunter(boba)
+// }
+
+// associateHan()
+
+// let lookUpHan = async () => {
+//     let han = await models.bounty.findOne({
+//         where: {
+//             name: 'Han Solo'
+//         }
+//     })
+//     const hansHunters = await han.getHunters()
+//     console.log(hansHunters)
+// }
+
+// lookUpHan()
+
+// let lookUpBoba = async () => {
+//     let boba = await models.hunter.findOne({
+//         where: {
+//             name: 'Boba Fett'
+//         }
+//     })
+//     const bobasBounties = await boba.getBounty()
+//     console.log(bobasBounties)
+// }
+
+// lookUpBoba()
+
+// let associateHanDengar = async () => {
+//     let han = await models.bounty.findOne({
+//         where: {
+//             name: 'Han Solo'
+//         }
+//     })
+//     let dengar = await models.hunter.findOne({
+//         where: {
+//             name: 'Dengar'
+//         }
+//     })
+//     han.addHunter(dengar)
+// }
+
+// associateHanDengar()
+
+// let lookUpHan = async () => {
+//     let han = await models.bounty.findOne({
+//         where: {
+//             name: 'Han Solo'
+//         }
+//     })
+//     const hansHunters = await han.getHunters()
+//     console.log(hansHunters)
+// }
+
+// lookUpHan()
+
+let lookUpDengar = async () => {
+    let dengar = await models.hunter.findOne({
+        where: {
+            name: 'Dengar'
+        }
+    })
+    const dengarsBounties = await dengar.getBounty()
+    console.log(dengarsBounties)
+}
+
+lookUpDengar()

1d6f4111e411c10f345212deeb1fa214b24ce0c9
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-03-31 22:33:44 -0400

    finished hw

--- dbTest.js
+++ dbTest.js
@@ -19,16 +19,16 @@ class Hunters {
 const boba = new Hunters('Boba Fett','Jabba the Hut', true);
 const dengar = new Hunters('Dengar', 'Mercurial Swift', false);
 
-// const makeHunters = () => { 
-// huntersArray.forEach(function(h){
-//     models.hunter.create(h)
-// })}
+const makeHunters = () => { 
+huntersArray.forEach(function(h){
+    models.hunter.create(h)
+})}
 // makeHunters();
 
-// const getAllBounties = async () =>{
-//     const allBounties = await models.bounty.findAll()
-//     console.log(allBounties);
-// }
+const getAllBounties = async () =>{
+    const allBounties = await models.bounty.findAll()
+    console.log(allBounties);
+}
 // getAllBounties()
 
 const getAllHunters = async () =>{
@@ -65,4 +65,56 @@ const deleteDengar = async () =>{
 const rudeeeee = () =>{
     models.hunter.create(dengar)
 }
-rudeeeee();
+// rudeeeee();
+
+// Is this relationship 1-to-many or many-to-many?
+// 1 to many
+
+// For this kind of relationship, do you need a foreign key or a join table?
+// foreign key
+
+// Which table should get the foreign key?
+// hunters
+
+// What should the foreign key be called?
+// bountyId
+
+// What data type should the foreign key be?
+// integer
+
+const bobbaGetHans = async () => {
+    const hans = await models.bounty.findOne({where:{name:'Hans Solo'}});
+    const bobba = await models.hunter.findOne({where:{name:'Boba Fett'}});
+
+    hans.addHunters(bobba)
+}
+// bobbaGetHans()
+
+const getHansHunters = async () => {
+    const hans = await models.bounty.findOne({where:{name:'Hans Solo'}});
+    const hansHunters =  await hans.getHunters();
+    console.log(hansHunters);
+}
+// getHansHunters()
+
+const getBobbasBounty = async () => {
+    const bobba = await models.hunter.findOne({where:{name:'Boba Fett'}});
+    const bobbasBounty = await bobba.getBounty()
+    console.log(bobbasBounty);
+}
+// getBobbasBounty()
+
+const dengarGetHans = async () => {
+    const hans = await models.bounty.findOne({where:{name:'Hans Solo'}});
+    const dengar = await models.hunter.findOne({where:{name:'Dengar'}});
+
+    hans.addHunters(dengar);
+}
+// dengarGetHans();
+
+const getDengarsBounty = async () => {
+    const dengar = await models.hunter.findOne({where:{name:'Dengar'}});
+    const dengarsBounty = await dengar.getBounty()
+    console.log(dengarsBounty);
+}
+// getDengarsBounty();

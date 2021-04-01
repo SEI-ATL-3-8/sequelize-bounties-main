8e27dbdd8aa8287ca8edc65b9de71760dbe3fa76
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-03-31 22:41:49 -0400

    anel homework bounties done

---
+++ migrations/20210401011358-add-bountyId-to-hunters.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    //  * Add altering commands here.
+    //  *
+    //  * Example:
+     await queryInterface.addColumn('hunters', 'bountyId', { type: Sequelize.DataTypes.INTEGER })
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
